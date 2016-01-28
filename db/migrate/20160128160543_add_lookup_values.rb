7b2d1ff455e876ee70ce35281ec65f22c9bca246
Blake Hyde <bhyde@on-site.com>
2016-01-28 12:36:12 -0500

    Fix the lists of lookup values...

--- db/migrate/20160128160543_add_lookup_values.rb
+++ db/migrate/20160128160543_add_lookup_values.rb
@@ -1,44 +1,53 @@
 class AddLookupValues < ActiveRecord::Migration
   VALUES = {
-    IncomeType => %w{
-      Employment
-      SSI/SSDI
-      EDD
-      None
-      Permanently Disabled
-      Temporarily Disabled
-    },
-    ResidenceType => %w{
-      Room
-      Apartment
-      Single family home
-      Town home
-    },
-    SubsidyType => %w{
-      Housing 1000
-      Section 8 voucher
-      HUD-VASH
-    },
-    ReasonType => %w{
-      Injury
-      Illness
-      Death in the family
-      Natural disaster
-      Currently homeless
-      Moving from unsuitable living conditions
-      Victim of crime
-      Temporary loss of employment
-      Rent increase unaffordable
-      No-fault notice to vacate
-      Unanticipated expenses
-      Moving from shelter
-      Delay/cancellation of assistance or benefits
-      Other
-    }
+    IncomeType => [
+      "Employment",
+      "SSI/SSDI",
+      "EDD",
+      "None",
+      "Permanently Disabled",
+      "Temporarily Disabled"
+    ],
+    ResidenceType => [
+      "Room",
+      "Apartment",
+      "Single family home",
+      "Town home"
+    ],
+    SubsidyType => [
+      "Housing 1000",
+      "Section 8 voucher",
+      "HUD-VASH"
+    ],
+    ReasonType => [
+      "Injury",
+      "Illness",
+      "Death in the family",
+      "Natural disaster",
+      "Currently homeless",
+      "Moving from unsuitable living conditions",
+      "Victim of crime",
+      "Temporary loss of employment",
+      "Rent increase unaffordable",
+      "No-fault notice to vacate",
+      "Unanticipated expenses",
+      "Moving from shelter",
+      "Delay/cancellation of assistance or benefits",
+      "Other"
+    ],
+    CoverageType => [
+      "Current month's rent",
+      "Past due rent (not including current month)",
+      "Security Deposit (prior to move-in)",
+      "Security Deposit (past-due, after move-in)",
+      "Utilities"
+    ]
   }
 
   def up
     VALUES.each do |klass, descriptions|
+      klass.delete_all
+
       descriptions.each do |desc|
         klass.create(description: desc)
       end
