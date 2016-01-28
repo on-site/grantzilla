36f53287753dde143608254dd76eefb975394efd
Joseph Jaber <mail@josephjaber.com>
2016-01-28 09:21:03 -0800

    Collect a Case Workers agency on sign up and display in Grants Review

---
+++ app/assets/javascripts/components/AgencySignupFields.jsx
@@ -0,0 +1,42 @@
+class AgencySignupFields extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { disableAgency: false }
+    this.emailChanged = this.emailChanged.bind(this);
+  }
+
+  emailChanged(e) {
+    this.forceUpdate();
+
+    if(e.target.value.indexOf('@hifinfo.org') > -1) {
+      this.setState({disableAgency: true});
+    } else {
+      this.setState({disableAgency: false});
+    }
+  }
+
+  render() {
+    let disableAgency = this.state.disableAgency;
+    let agencySelectOptions = this.props.agencies.map((agency) => {
+      return (
+        <option  key={agency.id} value={agency.id}>{agency.name}</option>
+      )
+    });
+
+    return (
+      <div>
+        <div className="form-group">
+          <label for="user_email">Email</label>
+          <input className="form-control" type="email" name="user[email]" onChange={this.emailChanged} id="user_email"/>
+        </div>
+        <div className="form-group">
+          <label for="user_agency_id">Agency</label>
+          <select disabled={disableAgency} className="form-control" name="user[agency_id]" id="user_agency_id">
+            <option value=""></option>
+            {agencySelectOptions}
+          </select>
+        </div>
+      </div>
+    );
+  }
+}
