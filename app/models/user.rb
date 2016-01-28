093ad08beaf260ef340a72a287ead394f79a12a1
Joseph Jaber <mail@josephjaber.com>
2016-01-27 20:14:14 -0800

    Clean up formatting of devise declaration in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   def full_name
     "#{first_name} #{last_name}"
