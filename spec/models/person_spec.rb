37f9dd637db4ca00955817d58aed923939487de3
Scott Jones <scottajones@gmail.com>
2016-01-28 00:43:39 -0800

    Stop using doubles but don't touch the database

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,17 +1,12 @@
 require "rails_helper"
 
-def create_income(current, disabled, monthly_income)
-  instance_double(Income, current: current, disabled: disabled, monthly_income: monthly_income)
-end
-
 RSpec.describe Person, type: :model do
   let(:person) { Person.new }
   context "current income methods" do
     before do
-      allow(person).to receive(:incomes).and_return [
-        create_income(false, false, 1000),
-        create_income(true, false, 2000),
-        create_income(true, true, 3000)]
+      person.incomes.build(current: false, disabled: false, monthly_income: 1000)
+      person.incomes.build(current: true, disabled: false, monthly_income: 2000)
+      person.incomes.build(current: true, disabled: true, monthly_income: 3000)
     end
     describe "#current_earned_income" do
       it "calculates correctly" do
