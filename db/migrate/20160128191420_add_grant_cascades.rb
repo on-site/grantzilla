056b8edc8080132745121da2ce3ac53b6d8cebf0
Blake Hyde <bhyde@on-site.com>
2016-01-28 14:30:08 -0500

    Issue 107: Add cascades for FKs pointing to Grant.

---
+++ db/migrate/20160128191420_add_grant_cascades.rb
@@ -0,0 +1,31 @@
+class AddGrantCascades < ActiveRecord::Migration
+  TABLES = %w{
+    grant_coverage_types
+    grant_reasons
+    other_payments
+    people
+  }
+
+  def remove_and_add(cascade = true)
+    on_delete = cascade ? :cascade : :restrict
+
+    TABLES.each do |t|
+      remove_foreign_key(t, :grants)
+      add_foreign_key(t, :grants, on_delete: on_delete)
+    end
+  end
+
+  def up
+    remove_and_add
+
+    remove_foreign_key(:incomes, :people)
+    add_foreign_key(:incomes, :people, on_delete: :cascade)
+  end
+
+  def down
+    remove_and_add(false)
+
+    remove_foreign_key(:incomes, :people)
+    add_foreign_key(:incomes, :people)
+  end
+end
