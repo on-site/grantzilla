d6211f43c2035bbd5415677d6781b310fc31ebd2
Joseph Jaber <mail@josephjaber.com>
2016-01-27 19:58:11 -0800

    Add first name and last name fields to User model
    Fixes #82

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128001156) do
+ActiveRecord::Schema.define(version: 20160128014528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -172,6 +172,8 @@ ActiveRecord::Schema.define(version: 20160128001156) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.string   "role",                   default: "user", null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
