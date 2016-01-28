f5d4cdcd146186fa46e14433d445d5b826b044e2
Scott Jones <scottajones@gmail.com>
2016-01-27 22:26:27 -0800

    Create budget edit view

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,8 @@ class Person < ActiveRecord::Base
   has_many :employments
   has_many :incomes
   belongs_to :grant
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :grants
+  resources :grants do
+    resources :budgets, only: [:index]
+  end
 
   root 'grants#index'
 end
