64ab3817735302b84b85f18d6ef644b00c5965d6
Joseph Jaber <mail@josephjaber.com>
2016-01-27 20:23:04 -0800

    Make 'case_worker' the default User role

--- db/schema.rb
+++ db/schema.rb
@@ -195,19 +195,19 @@ ActiveRecord::Schema.define(version: 20160128031635) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",     null: false
-    t.string   "encrypted_password",     default: "",     null: false
+    t.string   "email",                  default: "",            null: false
+    t.string   "encrypted_password",     default: "",            null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,      null: false
+    t.integer  "sign_in_count",          default: 0,             null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.string   "role",                   default: "user", null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "role",                   default: "case_worker", null: false
     t.string   "first_name"
     t.string   "last_name"
   end
