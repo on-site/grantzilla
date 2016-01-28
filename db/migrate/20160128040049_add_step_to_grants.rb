e51246ec9b58de36afee74608ae4858b09bdab89
Pierre Lebrun <anthonylebrun@gmail.com>
2016-01-27 20:59:49 -0800

    Removed default value for grant.step

--- db/migrate/20160128040049_add_step_to_grants.rb
+++ db/migrate/20160128040049_add_step_to_grants.rb
@@ -1,5 +1,5 @@
 class AddStepToGrants < ActiveRecord::Migration
   def change
-    add_column :grants, :step, :string, default: Grant::STEPS.first
+    add_column :grants, :step, :string
   end
 end
