e5dd04de9c826b744d6b9e84520f06c760667973
Pierre Lebrun <anthonylebrun@gmail.com>
2016-01-27 19:45:10 -0800

    Cleaned up seeds.rb with constant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-["Injury/Illness/Death in Family", "Loss of employment", "Moving from temporary/unsuitable living conditions",
-  "Currently honeless", "Rent increase unaffordable", "Victim of crime/natural disaster",
-  "Delay/cancellation/change in subsidy, assistance, or benefits", "No-fault notice to vacate",
-  "Unanticipated expenses"]
-  .each do |criteria|
-    ReasonType.create(description: criteria)
-  end
+QUALIFYING_CRITERIA = [
+  "Injury/Illness/Death in Family",
+  "Loss of employment",
+  "Moving from temporary/unsuitable living conditions",
+  "Currently honeless",
+  "Rent increase unaffordable",
+  "Victim of crime/natural disaster",
+  "Delay/cancellation/change in subsidy, assistance, or benefits",
+  "No-fault notice to vacate",
+  "Unanticipated expenses"
+]
+
+QUALIFYING_CRITERIA.each do |criteria|
+  ReasonType.create(description: criteria)
+end
+
