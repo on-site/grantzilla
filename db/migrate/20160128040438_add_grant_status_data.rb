f91fa1b12c748a7a6a528ce55c72a0e02346e949
Edward Anderson <nilbus@nilbus.com>
2016-01-27 21:55:41 -0800

    Set status to Incomplete for all new (and presently existing) grants

--- db/migrate/20160128040438_add_grant_status_data.rb
+++ db/migrate/20160128040438_add_grant_status_data.rb
@@ -1,11 +1,18 @@
+class GrantStatus < ActiveRecord::Base; end
+
 class AddGrantStatusData < ActiveRecord::Migration
-  def change
-    GrantStatus.create(description: "Incomplete")
+  def up
+    GrantStatus.delete_all
+    GrantStatus.create(description: "Incomplete", id: 1)
     GrantStatus.create(description: "Submitted")
     GrantStatus.create(description: "Approved")
     GrantStatus.create(description: "Denied")
     GrantStatus.create(description: "Paid")
     GrantStatus.create(description: "Refund Needed")
     GrantStatus.create(description: "Refund Received")
+  end
+
+  def down
+    GrantStatus.delete_all
   end
 end
