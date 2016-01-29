63b55f3ce4d67d7887e4909a27aba2d6154a8dbe
Blake Hyde <bhyde@on-site.com>
2016-01-29 11:54:28 -0500

    Add a bang to every let. (Why is this creating Grants locally but not on Travis?)

--- spec/models/grant_spec.rb
+++ spec/models/grant_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe Grant, type: :model do
   describe ".user_visible" do
-    let(:pending_status) { GrantStatus.where(pending: true).first }
-    let(:non_pending_status) { GrantStatus.where(pending: false).first }
+    let!(:pending_status) { GrantStatus.where(pending: true).first }
+    let!(:non_pending_status) { GrantStatus.where(pending: false).first }
 
     let!(:pending_grant) { Grant.create(status: pending_status) }
     let!(:non_pending_grant) { Grant.create(status: non_pending_status) }
 
-    let(:user_grants) { Grant.user_visible(user).map(&:id) }
+    let!(:user_grants) { Grant.user_visible(user).map(&:id) }
 
     context "when given a case worker" do
       let(:user) { double("case_worker?" => true) }
