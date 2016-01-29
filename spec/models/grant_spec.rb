5b542a918c16d290dc871d5b52ec5772d65441ab
Blake Hyde <bhyde@on-site.com>
2016-01-29 11:44:08 -0500

    Try a different creation format to try to fix Travis build.

--- spec/models/grant_spec.rb
+++ spec/models/grant_spec.rb
@@ -4,19 +4,18 @@ RSpec.describe Grant, type: :model do
   describe ".user_visible" do
     let(:pending_status) { GrantStatus.where(pending: true).first }
     let(:non_pending_status) { GrantStatus.where(pending: false).first }
-    let(:user_grants) { Grant.user_visible(user).map(&:id) }
 
-    before do
-      @grant1 = Grant.create(status: pending_status)
-      @grant2 = Grant.create(status: non_pending_status)
-    end
+    let!(:pending_grant) { Grant.create(status: pending_status) }
+    let!(:non_pending_grant) { Grant.create(status: non_pending_status) }
+
+    let(:user_grants) { Grant.user_visible(user).map(&:id) }
 
     context "when given a case worker" do
       let(:user) { double("case_worker?" => true) }
 
       it "includes only pending grants" do
-        expect(user_grants).to include(@grant1.id)
-        expect(user_grants).to_not include(@grant2.id)
+        expect(user_grants).to include(pending_grant.id)
+        expect(user_grants).to_not include(non_pending_grant.id)
       end
     end
 
@@ -24,8 +23,8 @@ RSpec.describe Grant, type: :model do
       let(:user) { double("case_worker?" => false) }
 
       it "includes all grants" do
-        expect(user_grants).to include(@grant1.id)
-        expect(user_grants).to include(@grant2.id)
+        expect(user_grants).to include(pending_grant.id)
+        expect(user_grants).to include(non_pending_grant.id)
       end
     end
   end
