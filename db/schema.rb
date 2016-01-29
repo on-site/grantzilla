8c9a563ea5f062fb428281dd2830202b1a1918d2
Blake Hyde <bhyde@on-site.com>
2016-01-28 19:42:44 -0500

    Add a migration to fix grants missing users, and users missing agencies.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128234922) do
+ActiveRecord::Schema.define(version: 20160129002837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
