f0c1762d6f101d91b67df15707bd79edcc53ebfc
Scott Jones <scottajones@gmail.com>
2016-05-09 08:54:13 -0500

    Add filters/search to user list
    * app/controllers/users_controller.rb: Call new User.list method
      rather than accessing scopes directly.
    
    * app/models/user.rb: Create new User.list method that uses a handful
      of new scopes to allow filtering on agency and to search.
    
    * app/views/users/index.html.erb: Change the message when no users are
    * found as we might have searched for something and not found it.
    
    * spec/models/user_spec.rb: Add tests to verify the search and
      filtering by agency work, as well as that we can find all users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,4 +50,28 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe "User.list" do
+    let(:agency1) { Agency.create }
+    let(:agency2) { Agency.create }
+    let(:worker1) { User.create first_name: "Fred", last_name: "Flintstone", email: "test1@test.com", password: "testtest", agency: agency1 }
+    let(:worker2) { User.create first_name: "Betty", last_name: "Rubble", email: "test2@test.com", password: "testtest", agency: agency2 }
+    before do
+      worker1
+      worker2
+    end
+    it "finds all users" do
+      expect(User.list.size).to eq(2)
+    end
+    it "allows searching by name" do
+      users = User.list(search: "flint")
+      expect(users.size).to eq(1)
+      expect(users).to include(worker1)
+    end
+    it "allows filtering by agency" do
+      users = User.list(agency_id: agency1.id)
+      expect(users.size).to eq(1)
+      expect(users).to include(worker1)
+    end
+  end
 end
