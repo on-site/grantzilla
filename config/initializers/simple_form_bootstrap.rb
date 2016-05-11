af39be34eae0afa62667bf6837efdb7d1c6c24d0
Scott Jones <scottajones@gmail.com>
2016-05-10 23:05:32 -0500

    Begin improving grant entry form
    * app/views/forms/applicants.html.erb: Change the header to
      "Applicants"
    
    * app/views/forms/fields/_person_fields.html.erb: Use a date field
      rather than 3 drop downs.  Use form-horizontal to make the form
      smaller.
    
    * config/initializers/simple_form_bootstrap.rb: For form-horizontal,
      use col-sm-4 and col-sm-8 rather than 3 and 9.
    
    * config/locales/en.yml: Change "Applicant Details" to "Applicants"

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -58,9 +58,9 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label, class: 'col-sm-3 control-label'
+    b.use :label, class: 'col-sm-4 control-label'
 
-    b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+    b.wrapper tag: 'div', class: 'col-sm-8' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
