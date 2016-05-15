997d9f8e05d1cce5e5806aae09cb3dca294ed840
Scott Jones <scottajones@gmail.com>
2016-05-15 09:05:32 -0500

    Only admins can view agencies
    * app/controllers/agencies_controller.rb: Only admins can view
      agencies.
    
    * spec/request/agencies_spec.rb: Add tests to verify that only admins
      can view/edit agencies.

---
+++ spec/request/agencies_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "Agencies", type: :request do
+  context "when the user is logged in" do
+    let(:user) do
+      User.create email: "test1@test.com", password: "testtest", role: role,
+                  confirmed_at: Time.zone.now, approved: true
+    end
+    let(:agency) { Agency.create }
+
+    before do
+      sign_in user
+    end
+
+    context "when the user is an admin" do
+      let(:role) { "admin" }
+      it "allows the user view the list of agencies" do
+        get "/agencies"
+        expect(response).to have_http_status(:success)
+      end
+      it "allows the user to view/edit an agency" do
+        get "/agencies/#{agency.id}"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "when the user is not an admin" do
+      let(:role) { "case_worker" }
+      it "gives a permission denied error when they attempt to view agencies" do
+        get "/agencies"
+        expect(response).to have_http_status(403)
+      end
+      it "gives a permission denied error when they attempt to edit other users" do
+        get "/agencies/#{agency.id}"
+        expect(response).to have_http_status(403)
+      end
+    end
+  end
+
+  context "when a user is not logged in" do
+    it "redirects them to sign in" do
+      get "/agencies"
+      expect(response).to redirect_to("/users/sign_in")
+    end
+  end
+end
