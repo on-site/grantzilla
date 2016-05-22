c448502623ed9c2b15f5d640b16db4df118797cc
Scott Jones <scottajones@gmail.com>
2016-05-22 16:20:56 -0500

    Send an email to the admin/case worker when comments are added
    * app/mailers/application_mailer.rb: Use the mailer layout.
    
    * app/mailers/comments_mailer.rb: New mailer for sending emails about
      comments.
    
    * app/models/comment.rb: When a comment gets created, send an email to
      the other involved user (the admin or the case worker).
    
    * app/models/user.rb: Add self.admins to get admins.
    
    * app/views/comments_mailer/comment_email.html.erb:
    * app/views/comments_mailer/comment_email.text.erb: Email templates
      for comments. The button used for the link is what is recommended by
      Email on Acid for working in all email clients.
    
    * app/views/layouts/mailer.html.erb:
    * app/views/layouts/mailer.text.erb: Layouts for our emails.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,23 @@
 class Comment < ActiveRecord::Base
   belongs_to :grant
   belongs_to :user
+
+  after_create :notify_users
+  default_scope { order(id: :desc) }
+
+  def context
+    grant.comments.where("id < ?", id)
+  end
+
+  def notify_users
+    users =
+      if user.admin?
+        [grant.user]
+      else
+        User.admins
+      end
+    users.each do |user|
+      CommentsMailer.comment_email(self, user).deliver
+    end
+  end
 end
