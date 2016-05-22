2acd34d8f195cf9788d0f9fdaf5573a969313f15
Scott Jones <scottajones@gmail.com>
2016-05-22 16:48:52 -0500

    Add more tests for comments
    * spec/factories.rb: Add factory for comments.
    
    * spec/models/comment_spec.rb: Add tests to verify that the context
      method works as expected.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Comment, type: :model do
   let(:admin) { create(:user, :admin) }
   let(:grant) { create(:grant, user: case_worker) }
   let(:comments_mail) { double("comment_mail", deliver: true) }
+
   describe "#create" do
     context "when the comment is created by the admin" do
       it "sends an email to the case worker" do
@@ -19,6 +20,28 @@ RSpec.describe Comment, type: :model do
       it "sends an email to the admin" do
         expect(CommentsMailer).to receive(:comment_email).with(anything, admin).and_return(comments_mail)
         grant.comments.create(user: case_worker, body: "Hello World")
+      end
+    end
+  end
+
+  describe "#context" do
+    let(:comment) { grant.comments.create(user: admin, body: "Work work work work work.") }
+    let(:context) { comment.context }
+    before do
+      allow(CommentsMailer).to receive(:comment_email).with(anything, anything).and_return(comments_mail)
+    end
+    context "when this is the only comment" do
+      it "returns an empty list" do
+        expect(context.size).to eq(0)
+      end
+    end
+    context "when there are other comments" do
+      let!(:comment1) { grant.comments.create(user: admin, body: "Hello?") }
+      let!(:comment2) { grant.comments.create(user: case_worker, body: "How are you?") }
+      it "returns only the other comments in reverse order" do
+        expect(context.size).to eq(2)
+        expect(context[0]).to eq(comment2)
+        expect(context[1]).to eq(comment1)
       end
     end
   end
