9f50d8984d5044380d6dbf4b16c0dcfe838f973e
Joe Dean <joe@on-site.com>
2016-07-05 14:36:15 -0700

    issue-206: Remove grant_id from uploads and use polymorphic user relationship
    * app/models/grant.rb: Use polymorphic relationship to access uploads
      from grant model.  Need to change the name of the polymorphic
      attribute.  "user" is a really confusing name since it makes you
      think of the system user instead of the model using the uploads
      schema.
    
    * db/migrate/20160705213210_remove_grant_id_reference_from_uploads.rb:
      Remove grant_id from uploads table.
    
    * db/schema.rb: Updated schema with removed grant_id column on uploads
      table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612215930) do
+ActiveRecord::Schema.define(version: 20160705213210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -234,10 +234,8 @@ ActiveRecord::Schema.define(version: 20160612215930) do
     t.datetime "file_updated_at",   null: false
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.integer  "grant_id"
   end
 
-  add_index "uploads", ["grant_id"], name: "index_uploads_on_grant_id", using: :btree
   add_index "uploads", ["user_type", "user_id"], name: "index_uploads_on_user_type_and_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -291,6 +289,5 @@ ActiveRecord::Schema.define(version: 20160612215930) do
   add_foreign_key "other_payments", "grants", on_delete: :cascade
   add_foreign_key "people", "grants", on_delete: :cascade
   add_foreign_key "residences", "residence_types"
-  add_foreign_key "uploads", "grants"
   add_foreign_key "users", "agencies"
 end
