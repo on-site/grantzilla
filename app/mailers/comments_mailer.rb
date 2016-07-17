be3d388f9ae201992ae95e21f179b708cf2b3923
Scott Jones <scottajones@gmail.com>
2016-07-17 11:06:40 -0500

    Fix up to_sentence which has moved to Array.

--- app/mailers/comments_mailer.rb
+++ app/mailers/comments_mailer.rb
@@ -2,6 +2,6 @@ class CommentsMailer < ApplicationMailer
   def comment_email(comment, user)
     @comment = comment
     mail to: user.email,
-         subject: "New comment on grant for #{@comment.grant.people.to_sentence}"
+         subject: "New comment on grant for #{@comment.grant.people.to_a.to_sentence}"
   end
 end
