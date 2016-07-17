f89cbf823fa6910888be161a738beca2217740d6
Scott Jones <scottajones@gmail.com>
2016-07-17 11:01:38 -0500

    Run rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_Grantzilla_session"
+Rails.application.config.session_store :cookie_store, key: '_grantzilla_session'
