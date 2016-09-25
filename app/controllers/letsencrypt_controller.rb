d6f927b6d312b35b4e9be8b2b70f8eb08e6dd24d
Joe Dean <joe@on-site.com>
2016-09-25 13:35:52 -0700

    issue-289: Add frozen_string-literal comment

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class LetsencryptController < ApplicationController
   def authenticate_key
     render text: "PVltCpMKHMFVvw2JnYNnsKovB8XNwbEm6iB_sjmTBvA.EOz8wpTt91fuDD0G0ziU0_tJHJNh11aZyXsVX3y7mro"
