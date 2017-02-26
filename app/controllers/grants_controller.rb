233d3e7c50f7c9c84b19b7c366ddcb056a0a1cd5
Kathy Lam <klam@on-site.com>
2017-02-26 13:25:12 -0800

    issue-304 Print application with all uploaded documents
    * app/controllers/grants_controller.rb
    **  Add download feature to create one pdf file which includes the
        application and all uploaded documents
    
    * app/views/grants/show.html.erb
    ** Add Download button
    
    * config/routes.rb
    ** Add route to handle download feature

--- app/controllers/grants_controller.rb
+++ app/controllers/grants_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 class GrantsController < ApplicationController
+  require 'rmagick'
+  include Magick
+
   before_action :authenticate_user!
-  before_action :set_grant, only: [:show, :edit, :update, :update_controls, :add_comment, :destroy]
+  before_action :set_grant, only: [:show, :edit, :update, :update_controls, :add_comment, :download_package, :destroy]
   before_action :set_controls_info, only: [:show, :edit]
 
   include PdfOptions
@@ -16,6 +19,19 @@ class GrantsController < ApplicationController
       format.pdf do
         render pdf_options(file_name: "grant_#{@grant.id}.#{@grant.updated_at.strftime('%Y-%m-%d')}",
                            debug: params[:debug].presence)
+      end
+    end
+  end
+
+  def download_package
+    respond_to do |format|
+      format.pdf do
+        pdf_filename_hash = filename_hash
+        render pdf_options(file_name: pdf_filename_hash[:application],
+                           debug: params[:debug].presence).merge(
+                             { save_to_file: pdf_filename_hash[:application], disposition: 'attachment' }
+                           )
+        create_all_docs_in_pdf(pdf_filename_hash)
       end
     end
   end
@@ -107,5 +123,34 @@ class GrantsController < ApplicationController
 
   def residence_attributes
     { residence_attributes: [:id, :residence_type_id, :address, :unit_number, :city, :state, :zip] }
+  end
+
+  def filename_hash
+    pdf_dir = "#{Rails.root}/pdfs"
+    Dir.mkdir(pdf_dir) unless File.exists?(pdf_dir)
+
+    {
+      application: "#{pdf_dir}/grant_#{@grant.id}.application.pdf",
+      package: "#{pdf_dir}/grant_#{@grant.id}.#{@grant.updated_at.strftime('%Y-%m-%d')}.pdf"
+    }
+  end
+
+  def create_all_docs_in_pdf(pdf_filename_hash)
+    one_pdf = Magick::ImageList.new(pdf_filename_hash[:application])
+
+    # Add the uploaded documents
+    add_uploaded_documents_from_aws(one_pdf)
+
+    one_pdf.write(pdf_filename_hash[:package])
+
+    # Delete the application pdf after it has been written along with the uploaded documents
+    File.delete(pdf_filename_hash[:application])
+  end
+
+  def add_uploaded_documents_from_aws(one_pdf)
+    # Example of how to add images
+    #   one_pdf.read("filename_with_path")
+
+    one_pdf
   end
 end
