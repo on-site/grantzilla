fa99872482c8a967b2f9e2ee5374f90540b8d31f
Kathy Lam <klam@on-site.com>
2017-03-27 22:45:46 -0700

    issue-330 Move the constants to the yml file

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,45 +11,6 @@ class Person < ApplicationRecord
   ADULT = "Adult"
   DEPENDENT = "Dependent"
 
-  DAYCARE = "Daycare"
-  FULL_TIME_STUDENT = "Full-Time Student"
-  PART_TIME_STUDENT = "Part-Time Student"
-  DEPENDENT_STUDENT_STATUSES = [DAYCARE, PART_TIME_STUDENT, FULL_TIME_STUDENT].freeze
-  STUDENT_STATUSES = [PART_TIME_STUDENT, FULL_TIME_STUDENT].freeze
-
-  AMERICAN_INDIAN_AND_ALASKA_NATIVE = "American_Indian and Alaska Native"
-  ASIAN = "Asian"
-  BLACK = "Black"
-  HISPANIC_AND_LATINO = "Hispanic and Latino"
-  MULTIRACIAL = "Multiracial"
-  NATIVE_HAWAIIAN_AND_OTHER_PACIFIC_ISLANDER = "Native Hawaiian and Other Pacific Islander"
-  WHITE = "White"
-  PREFER_NOT_TO_SAY = "Prefer Not to Say"
-  ETHNICITY = [AMERICAN_INDIAN_AND_ALASKA_NATIVE, ASIAN, BLACK, HISPANIC_AND_LATINO,
-               MULTIRACIAL, NATIVE_HAWAIIAN_AND_OTHER_PACIFIC_ISLANDER, WHITE,
-               PREFER_NOT_TO_SAY].freeze
-
-  EDUCATION_LESS_THAN_HIGH_SCHOOL = "Less than high school"
-  EDUCATION_HIGH_SCHOOL = "High school graduate (includes equivalency)"
-  EDUCATION_SOME_COLLEGE = "Some college, no degree"
-  EDUCATION_ASSOCIATE_DEGREE = "Associate's degree"
-  EDUCATION_BACHELOR_DEGREE = "Bachelor's degree"
-  EDUCATION_PHD = "Ph.D."
-  EDUCATION_GRADUATE_PROFESSIONAL_DEGREE = "Graduate or professional degree"
-  EDUCATION_LEVEL = [EDUCATION_LESS_THAN_HIGH_SCHOOL, EDUCATION_HIGH_SCHOOL,
-                     EDUCATION_SOME_COLLEGE, EDUCATION_ASSOCIATE_DEGREE,
-                     EDUCATION_BACHELOR_DEGREE, EDUCATION_PHD,
-                     EDUCATION_GRADUATE_PROFESSIONAL_DEGREE].freeze
-
-  GENDER_FEMALE = "Female"
-  GENDER_MALE = "Male"
-  GENDER_TRANSGENDER = "Transgender"
-  GENDER = [GENDER_FEMALE, GENDER_MALE, GENDER_TRANSGENDER].freeze
-
-  DISABILITY_NO = "No"
-  DISABILITY_YES = "Yes"
-  DISABILITY = [DISABILITY_NO, DISABILITY_YES].freeze
-
   def adult?
     person_type == ADULT
   end
