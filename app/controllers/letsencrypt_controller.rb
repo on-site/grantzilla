a7772e67e11ee0f2282dddb9d533921f5210c1be
Kathy Lam <klam@on-site.com>
2017-06-14 21:55:24 -0700

    issue-289 Update certificate

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class LetsencryptController < ApplicationController
   def authenticate_key
-    render text: "2bsG2XPdaI8vFFf2nLFH8IgCu1UBzBlN9PPLHEgGgC4.Yr_GjVvd1LZ8VhpCEWJJ4OY7t9QoHTilT8VztJg0v_w"
+    render text: "9gUAR3lrGe6MQ01Xxa4PnY7gGpvcYCIbGfgr2Ox2OF0.Yr_GjVvd1LZ8VhpCEWJJ4OY7t9QoHTilT8VztJg0v_w"
   end
 end
